8c680665b60add33c75cb97bfa7d9fbb2a31a62e
foram <forampatel6342@gmail.com>
2022-10-13 10:26:17 +0530

    Added my project

---
+++ Demo-app/Startup.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.Owin;
+using Owin;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+
+[assembly: OwinStartup(typeof(Demo_app.Startup))]
+
+namespace Demo_app
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
---
+++ Demo-app/obj/Debug/Package/PackageTmp/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="Demo_app.MvcApplication" Language="C#" %>
